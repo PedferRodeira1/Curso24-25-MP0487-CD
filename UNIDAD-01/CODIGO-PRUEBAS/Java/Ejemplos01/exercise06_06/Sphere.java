ea32bb84ed02fb73bf7d3bff0e6cdc006ba77533
PedferRodeira1 <pedromanuelfercha@iesrodeira.gal>
2024-11-26 18:25:40 +0100

    “Este es mi paso 7 del examen ud01 de Entornos de Desarrollo”

--- UNIDAD-01/CODIGO-PRUEBAS/Java/Ejemplos01/exercise06_06/Sphere.java
+++ UNIDAD-01/CODIGO-PRUEBAS/Java/Ejemplos01/exercise06_06/Sphere.java
@@ -7,7 +7,7 @@ public class Sphere {
    public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
 
-      System.out.print("Enter radius of sphere: ");
+      System.out.print("Entererer radius of sphere: ");
       double radius = input.nextDouble();
 
       System.out.printf("Volume is %f%n", sphereVolume(radius));
